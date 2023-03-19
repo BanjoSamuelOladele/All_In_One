48f1f1b9a3a13787923274e0e88f9c2be6d5bb19
OladeleSamuel <o.samuel@native.semicolon.africa>
2023-03-19 20:52:32 +0100

    assignment track

--- test/takeHome/BikeTest.java
+++ test/takeHome/BikeTest.java
@@ -16,6 +16,53 @@ class BikeTest {
         assertFalse(myRide.callBike());
     }
     @Test public void gearTest(){
+        myRide.bikeOn(true);
+        myRide.setGear("gear1");
+        assertEquals("gear1", myRide.getGear());
 
     }
+    @Test public void gearTest1(){
+        myRide.bikeOn(true);
+        myRide.setGear(("gear2"));
+        assertEquals("gear2", myRide.getGear());
+    }
+    @Test public void gearTest2(){
+        myRide.bikeOn(false);
+        myRide.setGear("gear1");
+        assertEquals("bike is switched off", myRide.getGear());
+    }
+    @Test public void gearTest3(){
+        myRide.bikeOn(true);
+        myRide.setGear("gear2");
+        assertEquals("gear2", myRide.getGear());
+    }
+    @Test public void accelerationTest(){
+        myRide.bikeOn(true);
+        myRide.setGear("gear1");
+        myRide.accelerated(11);
+        assertEquals("gear1", myRide.getGear());
+        assertEquals(12, myRide.getAccerated());
+    }
+    @Test public void accelerationTest1(){
+        myRide.bikeOn(true);
+        myRide.setGear("gear1");
+        myRide.accelerated(19);
+        assertEquals("gear1", myRide.getGear());
+        assertEquals(20, myRide.getAccerated());
+    }
+    @Test public void accelerationTest2(){
+        myRide.bikeOn(false);
+        myRide.setGear("gear1");
+        myRide.accelerated(20);
+        assertEquals("bike is switched off", myRide.getGear());
+        assertEquals(0, myRide.getAccerated());
+    }
+    @Test public void accelerationTest3(){
+        myRide.bikeOn(true);
+        myRide.setGear("gear2");
+        myRide.accelerated(21);
+        assertEquals("gear2", myRide.getGear());
+        assertEquals(23, myRide.getAccerated());
+    }
+
 }
