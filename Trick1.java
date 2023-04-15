8d66f8367546d8f43314d6cbfaa8749fb99afb5f
Oladele Samuel <98552318+Blurbeast@users.noreply.github.com>
2023-04-15 18:02:21 +0100

    Apr.15

---
+++ Trick1.java
@@ -0,0 +1,12 @@
+package trial;
+
+public class Trick1 {
+    public static void main(String[] args) {
+        int x =1;
+
+        for (int i = 0; i < 10; i++){
+            x = (int) Math.pow(3,2);
+        }
+        System.out.println(x);
+    }
+}
---
+++ YetANother.java
@@ -0,0 +1,21 @@
+package trial;
+import java.util.Scanner;
+public class YetANother {
+        public static void main(String[] args) {
+            int initialNumber;
+            int sum = 0;
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter the first number: ");
+            initialNumber = input.nextInt();
+
+            while(true){
+                System.out.print("Enter the summing number: ");
+                int tapIn = input.nextInt();
+                sum = sum + tapIn;
+                if(sum >= initialNumber) break;
+            }
+            System.out.println(initialNumber);
+            System.out.println(sum);
+        }
+    }
