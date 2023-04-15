0c908b8abc7691910dda8886895d7294dd5b4aab
Oladele Samuel <98552318+Blurbeast@users.noreply.github.com>
2023-04-15 18:03:43 +0100

    Apr.15

---
+++ NumberConcatenateTrianglr.java
@@ -0,0 +1,23 @@
+package trial;
+
+public class NumberConcatenateTrianglr {
+    public static void main(String[] args) {
+        int numberOfOccurence;
+        int numberOfOccurence2;
+        int numberOfOccurence3;
+        int numberOfOccurenc;
+
+        for (numberOfOccurenc = 0; numberOfOccurenc < 5; numberOfOccurenc++){
+            for (numberOfOccurence2 = 5; numberOfOccurence2 > numberOfOccurenc; numberOfOccurence2--){
+                System.out.print(" ");
+            }
+            for (numberOfOccurence = numberOfOccurence2; numberOfOccurence <= numberOfOccurence2; numberOfOccurence++){
+                int number = 1;
+                System.out.print(number);
+                number++;
+            }
+          //  for (numberOfOccurence3 )
+        }
+        System.out.println(" ");
+    }
+}
